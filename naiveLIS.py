8e662997a94d0ced24b7d0c04c09fc2074e4dba7
Geraldo Neto <122578684+Gezeroo@users.noreply.github.com>
2024-03-24 10:19:54 -0300

    Add files via upload

---
+++ naiveLIS.py
@@ -0,0 +1,33 @@
+from random import randint
+import time
+def naive(entrada, n):
+    if n == 1:
+        return 1
+    solucao = 1
+    for i in range(n - 1, -1, -1):
+        if entrada[i - 1] < entrada[n - 1]:
+            solucao = max(solucao, 1 + naive(entrada, i))
+    return solucao
+
+def naive_lis(entrada):
+    maximo = 1
+    n = len(entrada)
+    for i in range(n):
+        maximo = max(maximo, naive(entrada, n))
+    return maximo
+def gerar_entrada(n):
+    return [randint(0, 10000) for _ in range(n)]
+
+entrada = gerar_entrada(1000)
+with open('entrada.txt', 'w') as f:
+    f.write("entrada: ")
+    f.write(', '.join(map(str, entrada)))
+    f.write('\n')
+    f.write("solucao: ")
+    #medir tempo em miliseconds
+    inicio = time.time()
+    f.write(', '.join(map(str, str(naive_lis(entrada)))))
+    fim = time.time()
+    f.write('\n')
+    f.write("tempo: ")
+    f.write(str((fim - inicio) * 1000))
